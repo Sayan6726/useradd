c23498fee70504360e183bf6a04f7894d8fa2908
Sayan6726 <67683032+Sayan6726@users.noreply.github.com>
2022-09-14 15:03:31 +0530

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -9,7 +9,7 @@ if [ $(id -u) -eq 0 ]; then
                 exit 1
         else
                 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-                useradd -m -p "$pass" "$username" && mkdir -p /home/$username/.ssh && chown -R $username:$username /home/$username/.ssh && chmod 700 /home/$username/.ssh && touch /home/$username/.ssh/authorized_key && chown -R $username:$username /home/$username/.ssh/authorized_key && chmod 600 /home/$username/.ssh/authorized_key
+                useradd -m -p "$pass" "$username" && mkdir -p /home/$username/.ssh && chown -R $username:$username /home/$username/.ssh && chmod 700 /home/$username/.ssh && touch /home/$username/.ssh/authorized_key && chown -R $username:$username /home/$username/.ssh/authorized_keys && chmod 600 /home/$username/.ssh/authorized_key
                 [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
         fi
 else
